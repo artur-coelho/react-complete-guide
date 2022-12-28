82f7c3d3bf4db301d42196f4765b0de1028a0336
artur-coelho <artur.rocha.coelho@gmail.com>
2022-12-28 14:19:58 -0300

    created funcionality to show-hide form

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
 
 const NewExpense = (props) => {
+  const [formVisibility, setformVisibility] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    hideForm();
+  };
+
+  const showForm = () => {
+    setformVisibility(true);
+  };
+
+  const hideForm = () => {
+    setformVisibility(false);
   };
 
   return (
     <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {formVisibility ? (
+        <ExpenseForm
+          onCancel={hideForm}
+          onSaveExpenseData={saveExpenseDataHandler}
+        />
+      ) : (
+        <button onClick={showForm}>Add New Expense</button>
+      )}
     </div>
   );
 };
