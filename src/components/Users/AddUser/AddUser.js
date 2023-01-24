0df2f30a8b87d5c37a294e6da7ae5702408aa941
artur-coelho <artur.rocha.coelho@gmail.com>
2023-01-24 12:30:54 -0300

    created error modal

--- src/components/Users/AddUser/AddUser.js
+++ src/components/Users/AddUser/AddUser.js
@@ -2,12 +2,13 @@ import React, { useState } from 'react';
 
 import BaseCard from '../../UI/BaseCard/BaseCard';
 import Button from '../../UI/Button/Button';
+import ErrorModal from '../../UI/ErrorModal/ErrorModal';
 import styles from './AddUser.module.css';
 
 const AddUser = (props) => {
   const [username, setUsername] = useState('');
   const [age, setAge] = useState('');
-  const [isValid, setisValid] = useState(true);
+  const [error, setError] = useState();
 
   const usernameHandler = (event) => {
     setUsername(event.target.value);
@@ -19,11 +20,17 @@ const AddUser = (props) => {
   const addUserHandler = (event) => {
     event.preventDefault();
     if (username.trim().length === 0 || age.trim().length === 0) {
-      setisValid(false);
+      setError({
+        title: 'Invalid input',
+        message: 'Please enter a valid name or age (non-empty values)',
+      });
       return;
     }
     if (+age < 1) {
-      setisValid(false);
+      setError({
+        title: 'Invalid age',
+        message: 'Please enter a valid age ( >0 )',
+      });
       return;
     }
     props.addUser({
@@ -35,21 +42,34 @@ const AddUser = (props) => {
     setAge('');
   };
 
+  const closeModalHandler = () => {
+    setError(null);
+  };
+
   return (
-    <BaseCard className={styles.input}>
-      <form onSubmit={addUserHandler}>
-        <label htmlFor='username'>Username</label>
-        <input
-          type='text'
-          id='username'
-          value={username}
-          onChange={usernameHandler}
-        />
-        <label htmlFor='age'>Age</label>
-        <input type='number' id='age' value={age} onChange={ageHandler} />
-        <Button type='submit'>Add User</Button>
-      </form>
-    </BaseCard>
+    <div>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onCloseModal={closeModalHandler}
+        ></ErrorModal>
+      )}
+      <BaseCard className={styles.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor='username'>Username</label>
+          <input
+            type='text'
+            id='username'
+            value={username}
+            onChange={usernameHandler}
+          />
+          <label htmlFor='age'>Age</label>
+          <input type='number' id='age' value={age} onChange={ageHandler} />
+          <Button type='submit'>Add User</Button>
+        </form>
+      </BaseCard>
+    </div>
   );
 };
 
