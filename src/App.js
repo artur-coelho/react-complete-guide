b5e377c36bbe219b2e1ad0b1266302de8195fb62
artur-coelho <artur.rocha.coelho@gmail.com>
2023-03-06 11:02:46 -0300

    fetching data from api and loading

--- src/App.js
+++ src/App.js
@@ -1,20 +1,54 @@
-import React, { useContext } from 'react';
+import React, { useState } from 'react';
 
-import AuthContext from './store/auth-context';
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import MoviesList from './components/MoviesList';
+import './App.css';
 
 function App() {
-  const authCtx = useContext(AuthContext);
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false)
+
+  async function fetchMoviesHandler() {
+    setIsLoading(true)
+    const response = await fetch('https://swapi.dev/api/films/')
+    const data = await response.json()
+
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      }
+    })
+    setMovies(transformedMovies);
+    setIsLoading(false)
+  }
+
+  const dummyMovies = [
+    {
+      id: 1,
+      title: 'Some Dummy Movie',
+      openingText: 'This is the opening text of the movie',
+      releaseDate: '2021-05-18',
+    },
+    {
+      id: 2,
+      title: 'Some Dummy Movie 2',
+      openingText: 'This is the second opening text of the movie',
+      releaseDate: '2021-05-19',
+    },
+  ];
 
   return (
     <React.Fragment>
-      <MainHeader />
-      <main>
-        {!authCtx.isLoggedIn && <Login />}
-        {authCtx.isLoggedIn && <Home />}
-      </main>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length == 0 && <p>No movies found.</p>}
+        {isLoading && <p>Loading...</p>}
+      </section>
     </React.Fragment>
   );
 }
